ebb9d6964d349f284d843f9c4e6062a41d149135
Наталья Бугаева <natalia.bugaeva_sh13@mail.ru>
2024-06-02 22:15:52 +0700

    Вывод текущей даты

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -11,4 +11,29 @@ function showDate(){
     out3.innerHTML =  "English: "+today.toLocaleString("en");
     out4.innerHTML =  "Spanish: "+today.toLocaleString("es");
     out5.innerHTML =  "French: "+today.toLocaleString("fr");
-    out6.innerHTML =  "Indonesian: "+today.toLocaleString("id");}
+    out6.innerHTML =  "Indonesian: "+today.toLocaleString("id");
+
+    let year = document.getElementById('outputYear');
+    let month = document.getElementById('outputMonth');
+    let Day = document.getElementById('outputDay');
+    let weak = document.getElementById('outputWeak');
+    dt = new Date()
+    const months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+    const weakkk = ['Воскресенье', "Понедельник", "Вторник","Среда","Четверг","Пятница","Суббота"]
+    year.innerHTML = "Текущий год: " + dt.getFullYear()
+    month.innerHTML = "Текущий месяц: " + months[dt.getMonth()];
+    Day.innerHTML = "Текущий день: " + dt.getDate();
+    weak.innerHTML = "Текущий день недели: " + weakkk[dt.getDay()];
+}
+
+function showWeak(){
+  
+
+    let day = document.getElementById('day1').value
+    let month = document.getElementById('month1').value - 1
+    let year = document.getElementById('year1').value
+    let output = document.getElementById('outputweak2')
+    let dt = new Date(year,month,day);
+    let days = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота','Воскресенье']
+    output.innerHTML = days[dt.getDay()]
+}
